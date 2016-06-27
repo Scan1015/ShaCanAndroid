c033f6a20d2090ce0bbfd918a19ba374c5405a26
Scan1015 <1359854903@qq.com>
2016-06-27 13:54:59 +0800

    增加视图id

--- app/src/main/java/com/android/shacan/shacanandroid/MainActivity.java
+++ app/src/main/java/com/android/shacan/shacanandroid/MainActivity.java
@@ -2,13 +2,18 @@ package com.android.shacan.shacanandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.tv_text);
+
 
         //这是第一份
         //这是第二次
